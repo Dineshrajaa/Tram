ab153643a7105fb0ba8950e83cabe05434b39cac
Dineshrajaa <dineshrajaa.93@gmail.com>
2015-02-18 15:11:53 +0530

    Login,Register

--- www/js/tram.js
+++ www/js/tram.js
@@ -2,15 +2,18 @@ $(document).ready(function(){
 	var dbName;//Database Name
 
 		/**Start of DB Methods**/
+	//Method to Initialize Tables
 	function dbSetting(){
 		dbName.transaction(function(tx){
-			tx.executeSql("create table if not exists passengertable(pid integer primary key,pfname text,plname text,page integer,ppass text,pphone text,paddress text)");
+			//tx.executeSql("drop table passengertable");
+			tx.executeSql("create table if not exists passengertable(pid integer primary key,pfname text unique,plname text,page integer,ppass text,pphone text,paddress text)");
 			//alert("Created Ptable");
-			tx.executeSql("create table if not exists tramtable(tid integer primary key,tname text,tday text,ttime text,tsource text,tdestination text)");
+			tx.executeSql("create table if not exists tramtable(tid integer primary key,tname text unique,tday text,ttime text,tsource text,tdestination text)");
 			//alert("Created Ttable");
 		});
 	}
 
+	//Method to Register Passengers
 	function registerPassenger(){
 		var sfname=$("#fname").val();
 		var slname=$("#lname").val();
@@ -22,6 +25,29 @@ $(document).ready(function(){
 			tx.executeSql("insert into passengertable(pfname,plname,page,ppass,pphone,paddress) values(?,?,?,?,?,?)",[sfname,slname,sage,spass,smobile,saddress]);
 		});
 		toastAlert("Successfully Saved Profile");
+	}
+
+	//Method to validate and Login Passengers
+	function loginPassenger(){
+		//alert("Going to Login");
+		var lname=$("#uname").val();
+		var lpass=$("#upass").val();
+		dbName.transaction(function(tx){
+			tx.executeSql("select * from passengertable where pfname='"+lname+"' and ppass='"+lpass+"'",[],function(transaction,results){
+				//alert(results.rows.length);
+				if(results.rows.length>0) alert("Login Successful");
+				else alert("Check your Login Credentials");
+			});
+		});
+	}
+
+	//Method to validate and Loging Admin
+	function loginAdmin(){
+		
+		var alname=$("#aname").val();
+		var alpass=$("#apass").val();
+		if (alname=="admin" && alpass=="admin") alert("Welcome Admin");
+		else alert("Sorry you are not admin");
 	}
 
 		/**End of DB Methods**/
@@ -57,5 +83,9 @@ $(document).ready(function(){
 	});
 
 	$("#regbtn").tap(registerPassenger);
+
+	$("#loginbtn").tap(loginPassenger);
+
+	$("#adminloginbtn").tap(loginAdmin);
 	//Loaded all DOM elements
 });
